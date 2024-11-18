688355561c85af5c6b2db8fba971757587699e85
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-11-18 19:27:34 +0100

    showed db with json reading with api dedicated route and controller

--- laravel-new-exercise/app/Http/Controllers/api/WantedController.php
+++ laravel-new-exercise/app/Http/Controllers/api/WantedController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Models\Wanted;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class WantedController extends Controller
 {
-    //
+    public function index()
+    {
+        $wanted = Wanted::all();
+        return response()->json([
+            "success" => true,
+            "results" => $wanted,
+
+        ]);
+    }
 }
--- laravel-new-exercise/routes/api.php
+++ laravel-new-exercise/routes/api.php
@@ -19,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get("/wanted", [WantedController::class, "index"])->name("api.wanted.home");
